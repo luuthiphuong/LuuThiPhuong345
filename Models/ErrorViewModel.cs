b1fe96fafef4faaf20764e3148b7185e41bd18ce
luuthiphuong <luuphuong616@gmal.com>
2021-10-06 07:08:47 +0700

    first_commit_LTP345

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LuuThiPhuong345.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
